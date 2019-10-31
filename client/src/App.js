3bd6e246f163486ec73be4fd8e47d1b82c1a8f43
Ian Gallgher <igallag93@gmail.com>
2019-10-31 16:16:47 -0400

    added a front end component for registration and log in.  Not connected to Db

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+export default App;
